5da77d512211c9baa79595c8f7393c390fbe520c
Roman <romanpanv@gmail.com>
2022-10-06 15:44:06 +0300

    Добавляет две функции

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
-
 const getNumberInRange = function (min, max) {
   return min < 0 || min >= max ? NaN : Math.floor(Math.random() * (max - min + 1)) + min;
 };
 getNumberInRange(5,8);
 
-
 const checkStringLangth = (string, maxLength) => string.length <= maxLength;
 checkStringLangth('1sting', 11);
-
-
-
